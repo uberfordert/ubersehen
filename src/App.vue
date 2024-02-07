e48a862953b9ae23a28d59feea28efbef8026457
uberfordert <uberfordert@gmail.com>
2024-02-07 02:54:38 +0100

    1cewkjcn

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <SwiperGallery :products="productsData" />
+  </div>
+</template>
+
+<script>
+import SwiperGallery from './components/SwiperGallery.vue';
+
+export default {
+  components: {
+    SwiperGallery,
+  },
+  data() {
+    return {
+      productsData: [
+        {
+          id: 1,
+          imageUrl: require('./assets/ds-t1.png'),
+          alt: 'Ben & Ante'
+        },
+        {
+          id: 2,
+          imageUrl: require('./assets/ds-t2.png'),
+          alt: 'Ante & Ben'
+        }
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/SwiperGallery.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-span-5 lg:col-span-2 lg:col-start-2 col-start-1 flex items-center justify-center h-full bg-black">
+      <Swiper ref="swiperRef" class="w-full">
+        <SwiperSlide v-for="product in products" :key="product.id" class="flex items-center justify-center">
+          <img
+            :src="product.imageUrl"
+            :alt="product.alt"
+            class="mx-auto rounded-2xl"
+            style="max-width: 100%; max-height: 100vh;"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  </template>
+  
+  <script>
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    props: {
+      products: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
